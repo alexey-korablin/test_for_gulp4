aa550770e74de5b9f539c87d48aa04cd0d2e707c
Alex Korablin <alex8456@bk.ru>
2018-01-13 01:21:40 +0300

    gulp4-0001 Adds gulp-load-plugins, gulp-less and gulp-csso. Writes task for gulp-less

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 'use strict';
 
-let pug = require('gulp-pug');
+// let pug = require('gulp-pug');
 let gulp = require('gulp');
+let gp = require('gulp-load-plugins')();
 
 gulp.task('pug', () => {
     return gulp.src('src/pug/pages/*.pug')
-        .pipe(pug({
+        .pipe(gp.pug({
             pretty: true
         }))
         .pipe(gulp.dest('build'))
+})
+
+gulp.task('less', () => {
+    return gulp.src('src/static/less/*.less')
+        .pipe(gp.less({}))
+        .pipe(gp.csso())
+        .pipe(gulp.dest('build/static/css/'))
 })
