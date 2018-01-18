610ea07df22926f3aed39c05fbda722dad09222c
Alexey Korablin <Alexey_Korablin@epam.com>
2018-01-18 15:56:16 +0300

    gulp4-0002 Adds plugins gukp[autoprefixer, notify, sourcemaps] as dev-dependencies. Uses them in task 'less'. Writes two tasks: 'watch' and 'default'

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,26 @@ gulp.task('pug', () => {
 
 gulp.task('less', () => {
     return gulp.src('src/static/less/*.less')
+        .pipe(gp.sourcemaps.init())
         .pipe(gp.less({}))
+        .pipe(gp.autoprefixer({
+            browsers: ['last 10 versions'],
+            cascade: false
+        }))
+        .on("error", gp.notify.onError({
+            message: "Error: <%= error.message %>",
+            title: "Style"
+          }))
         .pipe(gp.csso())
+        .pipe(gp.sourcemaps.write())
         .pipe(gulp.dest('build/static/css/'))
 })
+
+gulp.task('watch', () => {
+    gulp.watch('src/pug/**/*.pug', gulp.series('pug')) // gulp.series('pug') 'pug' - it is name of task
+    gulp.watch('src/static/less/**/*.less', gulp.series('less'))
+})
+
+gulp.task('default', gulp.series(
+    gulp.parallel('pug', 'less'), 'watch'
+));
